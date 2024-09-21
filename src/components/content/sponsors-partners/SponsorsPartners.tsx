fd264dd8f6b9dc3002c698a182bdd6847f3e5d1a
Elcid-Chawa <elcidchawa@gmail.com>
2024-09-21 14:52:17 +0200

    added sponsor images

---
+++ src/components/content/sponsors-partners/SponsorsPartners.tsx
@@ -0,0 +1,23 @@
+import { sampleSponsorPartners } from "@/shared/data";
+import React from "react";
+import { SponsorsPartnerCard } from "./SponsorsPartnerCard";
+
+export const SponsorsPartners = () => {
+  return (
+    <div className="flex flex-col w-full space-y-10">
+      <h1 className="text-gnblueLight text-4xl ">Our Partners and Sponsors</h1>
+      <p className="text-white">
+        We collaborate with industry leaders and sponsors to drive innovation
+        and excellence in education. Their support and resources are integral to
+        our mission, enabling us to offer cutting-edge programs and
+        opportunities. Together, we shape the future and advance the goals of
+        our learners.
+      </p>
+      <div className="flex md:grid md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 overflow-scroll">
+        {sampleSponsorPartners.map((data) => (
+          <SponsorsPartnerCard data={data} />
+        ))}
+      </div>
+    </div>
+  );
+};
