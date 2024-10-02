1495150384f7d691dfd0f9398e93da8646a3c9d5
Elcid-Chawa <elcidchawa@gmail.com>
2024-10-02 07:42:33 +0200

    added cost to courses

--- src/shared/data/index.ts
+++ src/shared/data/index.ts
@@ -4,7 +4,7 @@ export const teamMembers: TeamMembers[] = [
   {
     names: "Theoneste NDAYISENGA",
     title: "Founder & CEO",
-    profileImage: "https://globalnexus.co.rw/images/Theoneste.png", 
+    profileImage: "https://globalnexus.co.rw/images/Theoneste.png",
   },
   {
     names: "Vedaste NYANDWI",
@@ -33,8 +33,6 @@ export const teamMembers: TeamMembers[] = [
   },
 ];
 
-
-
 export const carouselData = [
   {
     title: "Our Accreditations",
@@ -51,8 +49,6 @@ export const carouselData = [
   },
 ];
 
-
-
 export const sampleFeaturedCourses: Course[] = [
   {
     uuid: "8f1b2e19-3d3f-4740-bb93-d1da991b6969",
@@ -63,11 +59,11 @@ export const sampleFeaturedCourses: Course[] = [
     is_featured: false,
     lesson_count: 21,
     student_count: 11,
-    activity_count: 53,
+    activity_count: 76,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/2.jpg",
+    cost: 150,
   },
-
 
   {
     uuid: "7d158738-13c2-418f-a149-83035c30210c",
@@ -81,9 +77,8 @@ export const sampleFeaturedCourses: Course[] = [
     activity_count: 56,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/images.jpeg",
+    cost: 150,
   },
-
-
   {
     uuid: "77d6ff1d-5133-4ef4-822a-9b30c66f6c22",
     name: "Data Skills for Excel Professionals",
@@ -93,12 +88,11 @@ export const sampleFeaturedCourses: Course[] = [
     is_featured: false,
     lesson_count: 30,
     student_count: 2,
-    activity_count: 26,
+    activity_count: 30,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/alison.jpg",
+    cost: 150,
   },
-
-
   {
     uuid: "660fac45-d14b-4180-a866-b1807e43adc0",
     name: "Basics Python Data Analysis",
@@ -111,9 +105,8 @@ export const sampleFeaturedCourses: Course[] = [
     activity_count: 66,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/pythonf_for_data_science.png",
+    cost: 100,
   },
-
-
   {
     uuid: "cbc19e2d-507d-4596-b683-3a8555252489",
     name: "Python Data Analytics",
@@ -126,9 +119,8 @@ export const sampleFeaturedCourses: Course[] = [
     activity_count: 6,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/1682952919031.jpeg",
+    cost: 100,
   },
-
-
   {
     uuid: "077d3c58-9e6c-43ba-9ee8-a0c1f303fd18",
     name: "Data Analytics With Python and SQL",
@@ -138,12 +130,11 @@ export const sampleFeaturedCourses: Course[] = [
     is_featured: false,
     lesson_count: 10,
     student_count: 1,
-    activity_count: 16,
+    activity_count: 17,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/1705264155547.jpeg",
+    cost: 120,
   },
-
-
   {
     uuid: "2aed3935-1808-4f17-bc4e-30dc7d77edcd",
     name: "Data Science: Introduction",
@@ -156,6 +147,7 @@ export const sampleFeaturedCourses: Course[] = [
     activity_count: 12,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/3.jpg",
+    cost: 150,
   },
   {
     uuid: "476dacc0-d168-4a9c-9a19-b15a6c61d68b",
@@ -169,20 +161,36 @@ export const sampleFeaturedCourses: Course[] = [
     activity_count: 12,
     main_image:
       "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/Data_Science.jpg",
+    cost: 160,
   },
-  // {
-  //   uuid: "103bbba8-731f-412f-bb3d-74e0c6119443",
-  //   name: "Business Data Analytics",
-  //   slug: "business-data-analytics",
-  //   short_intro:
-  //     "Explore powerful tools and techniques to analyze and interpret data for business decision-making at our Business Data Analytics campus.",
-  //   is_featured: false,
-  //   lesson_count: 5,
-  //   student_count: 0,
-  //   activity_count: 2,
-  //   main_image:
-  //     "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/4.jpg",
-  // },
+  {
+    uuid: "1f47909e-36b1-43c6-88dd-6ca53afab755",
+    name: "Business Intelligence With PowerBI",
+    slug: "business-intelligence-with-powerbi",
+    short_intro:
+      "Unlock data insights with PowerBI! Learn to visualize, analyze, and make data-driven decisions in our Business Intelligence course.",
+    is_featured: false,
+    lesson_count: 12,
+    student_count: 0,
+    activity_count: 16,
+    main_image:
+      "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/images_1_1.jpeg",
+    cost: 150,
+  },
+  {
+    uuid: "103bbba8-731f-412f-bb3d-74e0c6119443",
+    name: "Business Data Analytics",
+    slug: "business-data-analytics",
+    short_intro:
+      "Explore powerful tools and techniques to analyze and interpret data for business decision-making at our Business Data Analytics campus.",
+    is_featured: false,
+    lesson_count: 5,
+    student_count: 0,
+    activity_count: 2,
+    main_image:
+      "https://prod-illumidesk-api-media-ueef29.s3.amazonaws.com/campuses/611abe06-3ac9-4a6f-81dc-f2abcb084623/4.jpg",
+    cost: 150,
+  },
 ];
 
 export const sampleSponsorPartners: SponsorsPartners[] = [
@@ -190,7 +198,7 @@ export const sampleSponsorPartners: SponsorsPartners[] = [
     title: "Sand Technologies",
     imageUrl: "/assets/images/sandtechnology.jpg",
     content:
-    "Global AI solutions company with expertise in enterprise and industrial AI, and data science.",
+      "Global AI solutions company with expertise in enterprise and industrial AI, and data science.",
   },
   {
     title: "ALX Africa",
