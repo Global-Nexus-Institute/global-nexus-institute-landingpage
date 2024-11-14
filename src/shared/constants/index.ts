c2d222a23a5901dedab940fbeb56237a4e31b654
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-14 09:18:05 +0200

    adding notification key

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -9,3 +9,4 @@ export const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
 export const messagingSenderId =
   process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
 export const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+export const vapId = process.env.NEXT_PUBLIC_VAPID_KEY;
