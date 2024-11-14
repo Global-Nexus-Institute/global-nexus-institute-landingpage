028c47256d5608b49987f82a32aed7a7dc4bddb0
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-14 09:15:44 +0200

    linking course to endpoint

--- src/components/courses/CourseContainer.tsx
+++ src/components/courses/CourseContainer.tsx
@@ -4,22 +4,30 @@ import { CourseCard } from "./CourseCard";
 import { Course } from "@/shared/types";
 import { sampleFeaturedCourses } from "@/shared/data";
 import Search from "antd/es/input/Search";
-import { Button } from "antd";
+import { Button, Spin } from "antd";
+import { useCourses } from "@/shared/hooks/courses/courses.hooks";
+import { useAppSelector } from "@/lib/store/store.hooks";
+import { RootState } from "@/lib/store/store";
 
 export default function CourseContainer() {
   const [courses, setCourses] = useState<Course[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const coursesPerPage = 8;
 
+  const { courses: courseList, loading } = useAppSelector((store: RootState) => store.courses);
+
+  const { getCourses } = useCourses();
+
   useEffect(() => {
     // Simulate data fetching
+    getCourses();
     setCourses(sampleFeaturedCourses);
   }, []);
 
   // Calculate the current courses to display
   const indexOfLastCourse = currentPage * coursesPerPage;
   const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
-  const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
+  const currentCourses = courseList ? courseList.slice(indexOfFirstCourse, indexOfLastCourse) : courses.slice(indexOfFirstCourse, indexOfLastCourse);
 
   // Handle page change
   const handleNextPage = () => {
@@ -57,13 +65,16 @@ export default function CourseContainer() {
         </div>
 
         {/* Courses Section */}
-        <div className="flex flex-col md:flex-row justify-center md:flex-wrap w-full gap-2">
+        <Spin spinning={loading} size="large" style={{ width: "100%" }} tip="Loading...">
+          <div className="flex flex-col md:flex-row justify-center md:flex-wrap w-full gap-2">
           {currentCourses.map((course: Course) => (
             <div className="flex h-[450px] card" key={course.uuid}>
               <CourseCard course={course} />
             </div>
           ))}
         </div>
+        </Spin>
+        
       </div>
     </div>
   );
