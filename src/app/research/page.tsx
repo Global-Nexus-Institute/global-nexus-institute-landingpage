0cfef9f92f2cbc67c32b8ebe2780ed555fa1dde5
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-18 17:39:42 +0200

    added page styles

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,7 +1,27 @@
+"use client";
 import React from "react";
 import "./research.css";
+import { List } from "antd";
+import { CheckOutlined } from "@ant-design/icons";
 
 export default function ResearchPage() {
+  const listItems = [
+    {
+      title: "Healthcare Data Analysis",
+      content:
+        "Data collection and cleaning for predictive analytics to improve healthcare outcomes and resource allocation.",
+    },
+    {
+      title: "Education & AI",
+      content:
+        "Visualization and advising support for students using AI to personalize learning in education research projects.",
+    },
+    {
+      title: "Agricultural Data Insights",
+      content:
+        "Visualization and data cleaning for climate-smart agriculture initiatives and crop yield predictions.",
+    },
+  ];
   return (
     <div className="mt-10 researh-body flex flex-col items-center justify-center">
       <header className="mt-20 font-bold w-full">
@@ -9,8 +29,10 @@ export default function ResearchPage() {
       </header>
       <div className="research-container">
         <section>
-          <h2 className="text-[2.5em] font-bold">Welcome to Our Research Services</h2>
-          <p className="text-[1.2em] text-white">
+          <h2 className="text-[2.5em] font-bold">
+            Welcome to Our Research Services
+          </h2>
+          <p className="text-[1.5em] text-white">
             Global Nexus Institute offers a range of research services designed
             to support individuals, businesses, and institutions. From data
             collection and cleaning to data visualization and student advising,
@@ -21,7 +43,7 @@ export default function ResearchPage() {
 
         <section>
           <h2 className="text-[2.5em] font-bold">Our Mission</h2>
-          <p className="text-[1.2em] text-white">
+          <p className="text-[1.5em] text-white">
             Our mission is to empower our clients with reliable research
             support, helping them turn data into actionable insights. We are
             dedicated to enhancing research capabilities and providing hands-on
@@ -34,7 +56,7 @@ export default function ResearchPage() {
           <div className="services-grid">
             <div className="service-box">
               <h3 className="text-[2em] font-bold">Data Collection</h3>
-              <p className="text-[1.2em]">
+              <p className="text-[1.3em]">
                 Comprehensive data gathering services using the latest
                 methodologies and tools, ensuring data accuracy and relevance
                 for your research goals.
@@ -42,7 +64,7 @@ export default function ResearchPage() {
             </div>
             <div className="service-box">
               <h3 className="text-[2em] font-bold">Data Cleaning</h3>
-              <p className="text-[1.2em]">
+              <p className="text-[1.3em]">
                 Professional data cleaning services to prepare your data for
                 analysis by handling inconsistencies, filling in missing values,
                 and ensuring data integrity.
@@ -50,7 +72,7 @@ export default function ResearchPage() {
             </div>
             <div className="service-box">
               <h3 className="text-[2em] font-bold">Data Visualization</h3>
-              <p className="text-[1.2em]">
+              <p className="text-[1.3em]">
                 Custom data visualization solutions to help you communicate your
                 findings effectively, using charts, graphs, and interactive
                 dashboards.
@@ -58,15 +80,17 @@ export default function ResearchPage() {
             </div>
             <div className="service-box">
               <h3 className="text-[2em] font-bold">Reporting</h3>
-              <p className="text-[1.2em]">
+              <p className="text-[1.3em]">
                 In-depth reporting solutions tailored to your research needs,
                 providing clear and actionable insights from complex data
                 analyses.
               </p>
             </div>
             <div className="service-box full-width-box">
-              <h3 className="text-[2em] font-bold">Student Support and Advising</h3>
-              <p className="text-[1.2em]">
+              <h3 className="text-[2em] font-bold">
+                Student Support and Advising
+              </h3>
+              <p className="text-[1.3em]">
                 We offer personalized support to students at all stages of their
                 research projects, from initial concept development to data
                 analysis and reporting. Our advisors provide expert guidance to
@@ -80,32 +104,35 @@ export default function ResearchPage() {
 
         <section>
           <h2 className="text-[2.5em] font-bold">Our Projects</h2>
-          <p className="text-[1.2em] text-white">
+          <p className="text-[1.5em] text-white">
             Explore some of our ongoing research projects and how our services
             support impactful outcomes:
           </p>
-          <ul className="text-white list-none">
-            <li>
-              <strong>Healthcare Data Analysis</strong>: Data collection and
-              cleaning for predictive analytics to improve healthcare outcomes
-              and resource allocation.
-            </li>
-            <li>
-              <strong>Education & AI</strong>: Visualization and advising
-              support for students using AI to personalize learning in education
-              research projects.
-            </li>
-            <li>
-              <strong>Agricultural Data Insights</strong>: Visualization and
-              data cleaning for climate-smart agriculture initiatives and crop
-              yield predictions.
-            </li>
-          </ul>
+          <List
+            itemLayout="vertical"
+            size="small"
+            dataSource={listItems}
+            renderItem={(item) => (
+              <List.Item key={item.title}>
+                <List.Item.Meta
+                  title={
+                    <span className="font-bold text-[1.5em] text-white">
+                      {item.title}
+                    </span>
+                  }
+                  avatar={<CheckOutlined style={{ color: "#2EB9C2", fontSize: "1.5em"  }} />}
+                  description={
+                    <p className="text-[1.3em] text-white">{item.content}</p>
+                  }
+                />
+              </List.Item>
+            )}
+          />
         </section>
 
         <section>
           <h2 className="text-[2.5em] font-bold">Collaborate with Us</h2>
-          <p className="text-[1.2em] text-white">
+          <p className="text-[1.5em] text-white">
             We welcome partnerships with educational institutions, research
             organizations, and businesses. For collaboration inquiries or to
             learn more about our services, please contact us at{" "}
